299181a6af8a34d61f07bd6830de71d92b7d3f68
Matheus Souza <matheu.sk.9@hotmail.com>
2018-06-28 00:21:27 -0300

    Create method that makes grids

--- designs.js
+++ designs.js
@@ -1,13 +1,42 @@
 // Select color input
-let color = $("#colorPicker").val();
+/*let color = $("#colorPicker").val();
 // Select size input
 let height = $("#inputHeight").val();
-let width = $("#inputWeight").val();
+let width = $("#inputWeight").val();*/
 
 // When size is submitted by the user, call makeGrid()
 
-function makeGrid() {
+class Grid {
+	/*constructor (height, width, button, element) {
+		this.height = height;
+		this.width = width;
+	}*/
+
+
+
+	makeGrid() {
+		$("#sizePicker").one("submit", function(e) {
+			let height = $("#inputHeight").val();
+			let width = $("#inputWeight").val();
+			var vertical;
+			for (var i = 1; i <= height; i++) {
+				var vertical = document.createElement("tr");
+				for (var j = 1; j <= width; j++) {
+					var horizontal = document.createElement("td");
+					vertical.append(horizontal);
+					$("#pixelCanvas").append(vertical);
+				}
+			};
+			e.preventDefault();
+		})
+	}
+}
+
+let creation = new Grid();
+creation.makeGrid();
+
+/*function makeGrid() {
 
 // Your code goes here!
 
-}
+}*/
